d4d8da68b90f6f4dd7c7823607e6676dc51cf750
ujjwal270700c <ujjwal270700c@gmail.com>
2021-01-10 16:55:03 +0530

    connt

--- src/db/conn.js
+++ src/db/conn.js
@@ -1,25 +1,26 @@
-// const mongoose=require('mongoose')
+const mongoose=require('mongoose')
+require('dotenv').config();
 
-// mongoose.connect("mongodb+srv://dbUser:ujjwal1944@cluster0.on2lp.mongodb.net/slack_clone?retryWrites=true&w=majority",{
-//     useCreateIndex:true,
-//     useNewUrlParser:true,
-//     useUnifiedTopology:true,
-//     useFindAndModify:true
-// }).then(()=> console.log("connection sucessful")).catch((err)=> console.log(err));
-const mongoose = require('mongoose')
-const url = 'mongodb://localhost/comments'
-
-mongoose.connect(url, {
-    useNewUrlParser:true,
-    useUnifiedTopology: true,
-    useFindAndModify: true,
+mongoose.connect(process.env.uri,{
     useCreateIndex:true,
-    
-})
+    useNewUrlParser:true,
+    useUnifiedTopology:true,
+    useFindAndModify:true
+}).then(()=> console.log("connection sucessful")).catch((err)=> console.log(err));
+// const mongoose = require('mongoose')
+// //const url = 'mongodb://localhost/comments'
 
-const connection = mongoose.connection
-connection.once('open', function() {
-    console.log('Database connected...')
-}).catch(function(err){
-    console.log('Connection failed...')
-})
+// mongoose.connect(process.env.uri, {
+//     useNewUrlParser:true,
+//     useUnifiedTopology: true,
+//     useFindAndModify: true,
+//     useCreateIndex:true,
+    
+// })
+
+// const connection = mongoose.connection
+// connection.once('open', function() {
+//     console.log('Database connected...')
+// }).catch((err)=>{
+//     console.log('Connection failed...')
+// })
