b8b8762d28a4fbacf7774ac1125646ac246e1433
ujjwal270700c <ujjwal270700c@gmail.com>
2021-01-10 16:25:40 +0530

    conn

--- src/db/conn.js
+++ src/db/conn.js
@@ -1,8 +1,25 @@
-const mongoose=require('mongoose')
+// const mongoose=require('mongoose')
 
-mongoose.connect("mongodb+srv://dbUser:ujjwal1944@cluster0.on2lp.mongodb.net/slack_clone?retryWrites=true&w=majority",{
+// mongoose.connect("mongodb+srv://dbUser:ujjwal1944@cluster0.on2lp.mongodb.net/slack_clone?retryWrites=true&w=majority",{
+//     useCreateIndex:true,
+//     useNewUrlParser:true,
+//     useUnifiedTopology:true,
+//     useFindAndModify:true
+// }).then(()=> console.log("connection sucessful")).catch((err)=> console.log(err));
+const mongoose = require('mongoose')
+const url = 'mongodb://localhost/comments'
+
+mongoose.connect(url, {
+    userNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: true,
     useCreateIndex:true,
-    useNewUrlParser:true,
-    useUnifiedTopology:true,
-    useFindAndModify:true
-}).then(()=> console.log("connection sucessful")).catch((err)=> console.log(err));
+    
+})
+
+const connection = mongoose.connection
+connection.once('open', function() {
+    console.log('Database connected...')
+}).catch(function(err){
+    console.log('Connection failed...')
+})
